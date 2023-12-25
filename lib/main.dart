b50cd130f9f121ab9091345c28ad06630cb88cc1
Hasith manohara <hasithamanohara315@gmail.com>
2023-12-25 08:58:32 +0530

    layout_two

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,66 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body:Column(children: [
-          Container(height: 100,width: 100,color: Colors.red),
-        ],),
+      home: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.black,
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.red,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 170,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 170,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+              Container(
+                width:double.infinity,
+                height: 100,
+                color: Colors.red,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: 110,
+                    height: 100,
+                    color: Color.fromARGB(255, 134, 47, 192),
+                  ),
+                  Container(
+                    width: 110,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    width: 110,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                ],
+              ),
+              Container(
+                width: double.infinity,
+                height: 100,
+                color: Colors.red,
+              ),
+            ],
+          ),
         ),
-      );
+      ),
+    );
   }
 }
